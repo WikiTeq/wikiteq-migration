74d4170a9eee7f9150f940965ad11505f3c65a61
Vedmaka <god.vedmaka@gmail.com>
2025-12-03 23:53:56 +0100

    Adds healthchecks.io support

---
+++ healthchecks-ping.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$H_PING_KEY" ] || [ -z "$H_SLUG" ] || [ -z "$H_SLUG_SUFFIX" ]; then
+    # nothing to do if the key is not set
+    exit 0
+fi
+
+echo "Pinging healthchecks.io..."
+curl -m 10 --retry 5 "https://hc-ping.com/$H_PING_KEY/$H_SLUG$H_SLUG_SUFFIX?create=1"
+echo "Done."
