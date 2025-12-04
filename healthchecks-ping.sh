9f8eb3126737fd276f27299f24bbf960d51aab05
Vedmaka <god.vedmaka@gmail.com>
2025-12-04 13:33:00 +0100

    Add fallback to wget if curl is unavailable in healthchecks-ping.sh

--- healthchecks-ping.sh
+++ healthchecks-ping.sh
@@ -6,5 +6,9 @@ if [ -z "$H_PING_KEY" ] || [ -z "$H_SLUG" ] || [ -z "$H_SLUG_SUFFIX" ]; then
 fi
 
 echo "Pinging healthchecks.io..."
-wget --timeout=10 --tries=5 -qO- "https://hc-ping.com/${H_PING_KEY}/${H_SLUG}${H_SLUG_SUFFIX}?create=1"
+if command -v curl >/dev/null 2>&1; then
+    curl -m 10 --retry 5 "https://hc-ping.com/${H_PING_KEY}/${H_SLUG}${H_SLUG_SUFFIX}?create=1"
+else
+    wget --timeout=10 --tries=5 -qO- "https://hc-ping.com/${H_PING_KEY}/${H_SLUG}${H_SLUG_SUFFIX}?create=1"
+fi
 echo "Done."
